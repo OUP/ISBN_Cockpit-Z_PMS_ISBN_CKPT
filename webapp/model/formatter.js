5afda5fe1af23c6c2f1b9130abf5ab23eee966b9
Shanmugam Bhaskaran <shanmugam.bhaskaran@oup.com>
2021-08-03 06:12:28 +0000

    initial version

---
+++ webapp/model/formatter.js
@@ -0,0 +1,33 @@
+sap.ui.define([], function () {
+  "use strict";
+
+  return {
+    RAGIcon: (sRagStatus) => {
+      let sReturn = "";
+
+      if (sRagStatus === "R") {
+        sReturn = "sap-icon://status-negative";
+      } else if (sRagStatus === "A") {
+        sReturn = "sap-icon://status-inactive";
+      } else if (sRagStatus === "G") {
+        sReturn = "sap-icon://status-positive";
+      }
+
+      return sReturn;
+    },
+
+    RAGStatus: (sRagStatus) => {
+      let sReturn = sap.ui.core.ValueState.None;
+
+      if (sRagStatus === "R") {
+        sReturn = sap.ui.core.ValueState.Error;
+      } else if (sRagStatus === "A") {
+        sReturn = sap.ui.core.ValueState.Warning;
+      } else if (sRagStatus === "G") {
+        sReturn = sap.ui.core.ValueState.Success;
+      }
+
+      return sReturn;
+    },
+  };
+});
---
+++ webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "ISBN Cockpit",
+									"targetURL": "#ISBNCockpit-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"ISBNCockpit-display": {
+								"semanticObject": "ISBNCockpit",
+								"action": "display",
+								"description": "ISBN Cockpit Application",
+								"title": "ISBN Cockpit",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=oup.pms.zpmsisbnckpt",
+									"url": sap.ui.require.toUrl("oup/pms/zpmsisbnckpt")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
